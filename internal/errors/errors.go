800a111afbfad73867167c570a0d8546e4241774
Sebastião Casaleiro <sebastiancc.info@gmail.com>
2026-01-17 01:46:48 +0100

    Create custom errors
    - create InvalidMovieName error

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -2,6 +2,14 @@ package errors
 
 import "fmt"
 
+type InvalidMovieNameError struct {
+	Title string
+}
+
+func (e *InvalidMovieNameError) Error() string {
+	return fmt.Sprintf("Invalid movie name: [%s]\n", e.Title)
+}
+
 type MovieNotFoundError struct {
 	Title string
 }
